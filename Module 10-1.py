d84e3062b19a523d111fdcc966df18ea7a8e4b76
Amuro78 <Amuro0101@outlook.com>
2026-03-23 21:20:47 +0200

    Add files via upload

---
+++ Module 10-1.py
@@ -0,0 +1,59 @@
+#Module 10-1
+
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self):
+        if self.current_floor < self.top_floor:
+            self.current_floor += 1
+            print(f'Elevator moved up to floor {self.current_floor}.')
+            return
+
+
+        else:
+            print(f"Already at the top floor {self.top_floor}")
+            return
+
+
+
+    def floor_down(self):
+        if self.current_floor > self.bottom_floor:
+            self.current_floor -= 1
+            print(f'Elevator moved down to floor {self.current_floor}.')
+            return
+
+        else:
+            print(f"Already at the bottom floor {self.bottom_floor}")
+            return
+
+
+    def go_to_floor(self,target):
+        if target == self.current_floor:
+            print(f'We are arriving at floor {target}.')
+
+
+        print(f"\nMoving from floor {self.current_floor} to floor {target}:")
+
+        if target > self.current_floor:
+            while self.current_floor < target:
+                self.floor_up()
+            return
+
+
+        else:
+            while self.current_floor > target:
+                self.floor_down()
+            return
+
+
+
+#main program
+h = Elevator(1, 10)
+print("Basic elevator test:")
+h.go_to_floor(5)
+h.go_to_floor(1)
+
+
---
+++ Module 10-3.py
@@ -0,0 +1,83 @@
+#Module 10-3
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self):
+        if self.current_floor < self.top_floor:
+            self.current_floor += 1
+            print(f'Elevator moved up to floor {self.current_floor}.')
+            return
+
+
+        else:
+            print(f"Already at the top floor {self.top_floor}")
+            return
+
+
+
+    def floor_down(self):
+        if self.current_floor > self.bottom_floor:
+            self.current_floor -= 1
+            print(f'Elevator moved down to floor {self.current_floor}.')
+            return
+
+        else:
+            print(f"Already at the bottom floor {self.bottom_floor}")
+            return
+
+
+    def go_to_floor(self,target):
+        if target == self.current_floor:
+            print(f'We are arriving at floor {target}.')
+            return
+
+
+        print(f"\nMoving from floor {self.current_floor} to floor {target}:")
+
+        if target > self.current_floor:
+            while self.current_floor < target:
+                self.floor_up()
+            return
+
+
+        else:
+            while self.current_floor > target:
+                self.floor_down()
+            return
+
+class Building:
+    def __init__(self, bottom_floor, top_floor, num_elevators):
+        self.bottom_floor =bottom_floor
+        self.top_floor =top_floor
+        self.num_elevators =num_elevators
+        self.elevators=[]
+
+        for i in range(num_elevators):
+            elevator= Elevator(bottom_floor,top_floor)
+            self.elevators.append(elevator)
+
+        print(f"Floors {bottom_floor} to {top_floor},{num_elevators} elevators")
+
+    def run_elevator(self,elevator_number,destination_floor):
+
+        elevator = self.elevators[elevator_number]
+
+        print(f"Running Elevator {elevator_number} to floor {destination_floor}")
+        elevator.go_to_floor(destination_floor)
+
+
+    def fire_alarm(self):
+        print("\nFire alarm activated! All elevators returned to the bottom floor.")
+        for elevator in self.elevators:
+            elevator.go_to_floor(self.bottom_floor)
+        return
+
+
+building = Building(1, 10, 3)
+building.run_elevator(0, 5)
+building.run_elevator(1, 8)
+building.run_elevator(2, 3)
+building.fire_alarm()
